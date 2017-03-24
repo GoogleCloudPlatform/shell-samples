6fc8e56c05bec4230b8c8c0c7bc01993e6c20c51
Tim Swast <swast@google.com>
2017-03-24 14:22:02 -0700

    Use root user to SSH to instances.
    This should fix the tests on Jenkins.
    See: http://stackoverflow.com/a/29299246/101923

--- zone_failure/tests/test.sh
+++ zone_failure/tests/test.sh
@@ -50,7 +50,7 @@ echo "Got and IP: $IP"
 
 echo "Copy the failure.sh script to VM"
 for i in  {1..10} ; do
-  gcloud --project="$PROJECT" compute copy-files --zone="$ZONE" "$DIR/../failure.sh" "$INSTANCE_NAME:~/failure.sh" && rc=$? || rc=$?
+  gcloud --project="$PROJECT" compute copy-files --zone="$ZONE" "$DIR/../failure.sh" "root@$INSTANCE_NAME:~/failure.sh" && rc=$? || rc=$?
   echo "Sleeping 30 seconds"
   sleep 30
   if [[ $rc == 0 ]] ; then
@@ -65,7 +65,7 @@ done
 
 echo "Run the failure.sh script in the bg"
 for i in  {1..10} ; do
-  gcloud --project="$PROJECT" compute ssh --zone="$ZONE" "$INSTANCE_NAME" --command "sudo bash ~/failure.sh" &
+  gcloud --project="$PROJECT" compute ssh --zone="$ZONE" "root@$INSTANCE_NAME" --command "sudo bash ~/failure.sh" &
   ssh_pid=$!
   echo "Sleeping 30 seconds"
   sleep 30
